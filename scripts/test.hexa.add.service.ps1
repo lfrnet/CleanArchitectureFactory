2bf5b7abf5097d20e4abb79d815c2d97592c4abd
iventis.lribeiro <@P1ss1g2m>
2023-01-06 21:54:22 -0300

    ajustes

--- scripts/test.hexa.add.service.ps1
+++
@@ -1,2 +0,0 @@
-dotnet build "..\src\Silent.Tool.Hexagonal.Cli"
-..\src\Silent.Tool.Hexagonal.Cli\bin\Debug\netcoreapp3.1\Silent.Tool.Hexagonal.Cli.exe add service "Identity" --company "Antourage" --framework "net5.0"
--- scripts/test.hexa.config.list.ps1
+++
@@ -1,2 +0,0 @@
-dotnet build "..\src\Silent.Tool.Hexagonal.Cli"
-..\src\Silent.Tool.Hexagonal.Cli\bin\Debug\netcoreapp3.1\Silent.Tool.Hexagonal.Cli.exe config list
